5ff813657fa5f0196c5b638f7ac2594acff8dfab
KirtiTung <70936047+KirtiTung@users.noreply.github.com>
2023-05-09 22:20:21 +0530

    Add files via upload

---
+++ BankLendingPortal/src/main/java/com/kirti/portal/entities/Role.java
@@ -0,0 +1,15 @@
+package com.kirti.portal.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Role {
+	
+	@Id
+	private int id;
+	private String name;
+}
